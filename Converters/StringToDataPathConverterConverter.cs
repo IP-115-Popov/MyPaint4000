17d746907d74db33ac17734239837d1b13e639a0
IP-115-Popov <99480504+IP-115-Popov@users.noreply.github.com>
2023-04-02 21:05:26 +0700

    Add StringToDataPathConverterConverter

---
+++ Converters/StringToDataPathConverterConverter.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media;
+using MyPaint4000.ViewModels.Page;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPaint4000.Converters
+{
+    public class StringToDataPathConverterConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return Geometry.Parse((string)value);
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
