9fd2c8e2dccce8ef6b79047ef7eeb8469578fea9
IP-115-Popov <99480504+IP-115-Popov@users.noreply.github.com>
2023-04-03 20:32:14 +0700

    Add ITransformShape

---
+++ Models/ITransformShape.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPaint4000.Models
+{
+    public interface ITransformShape
+    {
+        //изменение координат
+        double TranslateTransformX { get; set; }
+        double TranslateTransformY { get; set; }
+        //угол поворота
+        double RotateTransformAngleDeg { get; set; }
+        //цетер во круг которого поворацивается
+        double RotateTransformCenterX { get; set; }
+        double RotateTransformCenterY { get; set; }
+        //маштабирование
+        double ScaleTransformX { get; set; }
+        double ScaleTransformY { get; set; }
+        //вытягивание по асям
+        //угол наклона по асям
+        double SkewTransformAngleX { get; set; }
+        double SkewTransformAngleY { get; set; }
+        //центр
+        double SkewTransformCenterX { get; set; }
+        double SkewTransformCenterY { get; set; }
+    }
+}
